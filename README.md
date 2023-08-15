757121b3f40c5d27807326e6df277717005b549f
Alán Dórame Jara <alan_dj93@hotmail.com>
2023-08-15 00:17:44 -0700

    Added sample scene and documentation files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Movement
+
+The movement copmonents allow you to easily craft physics based gameplay
+
+### Disclaimer
+
+This repository is under active development. Everything is subject to change. The `master` branch is our current development branch and may not work on the latest publicly available version of Unity.
+
+## Instructions
+
+After installation, just drag the components on your GameObjects
+
+**Requires Unity 2019.4.33f1 Beta**
+
+The best version
---
+++ Samples~/SamplesMovingCapsule/MovingCapsule.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MiCompa.Movement.Samples
+{
+    [RequireComponent(typeof(MovementBehaviour))]
+    public class MovingCapsule : MonoBehaviour
+    {
+        private MovementBehaviour _movementBehaviour;
+
+        private void Awake()
+        {
+            if (TryGetComponent(out MovementBehaviour movementBehaviour)) _movementBehaviour = movementBehaviour;
+        }
+        private void FixedUpdate()
+        {
+            _movementBehaviour.MoveInDirection(Vector3.forward);
+        }
+    }
+}
